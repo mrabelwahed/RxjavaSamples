1da44ae0f9b1150d78d36baa879eaabb15816ec0
Mahmoud Ramadan <eng.mahmoudramadan2012@gmail.com>
2018-03-19 14:26:38 +0200

    working on download image sample

--- app/src/main/java/com/example/mahmoud/rxdownload/RxDownloadActivity.java
+++ app/src/main/java/com/example/mahmoud/rxdownload/RxDownloadActivity.java
@@ -1,34 +1,33 @@
 package com.example.mahmoud.rxdownload;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.mahmoud.samples.R;
 import com.github.lzyzsd.circleprogress.ArcProgress;
 
+import java.io.BufferedInputStream;
 import java.io.File;
-import java.io.IOException;
-import java.util.concurrent.Callable;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import io.reactivex.Observable;
-import io.reactivex.ObservableSource;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-import okio.BufferedSink;
-import okio.Okio;
+import io.reactivex.subjects.PublishSubject;
 
 /**
  * Created by mahmoud on 15/03/18.
@@ -39,20 +38,14 @@ public class RxDownloadActivity extends AppCompatActivity {
     ArcProgress arcProgress;
     @BindView(R.id.download)
     Button downloadBtn;
-    @BindView(R.id.img)
-    ImageView imageView;
 
-    File folder, file;
-
-    private static final String IMAGE_URL = "http://i.dailymail.co.uk/i/pix/2015/11/16/20/2E82087500000578-3321104-image-a-29_1447706769339.jpg";
-    // private PublishSubject<Integer> progressValueObservabele = PublishSubject.create();
+    private PublishSubject<Integer> progressValueObservabele = PublishSubject.create();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rx_downloader);
         ButterKnife.bind(this);
-        folder = new File(Environment.getExternalStorageDirectory(), "/Downloads");
     }
 
 
@@ -61,40 +54,92 @@ public class RxDownloadActivity extends AppCompatActivity {
         downloadBtn.setClickable(false);
         downloadBtn.setTextColor(Color.parseColor("#f7f7f7"));
 
-        Request request = new Request.Builder().url(IMAGE_URL).build();
-        Observable.defer(new Callable<ObservableSource<?>>() {
-            @Override
-            public ObservableSource<?> call() throws Exception {
-                try {
-                    Response response = new OkHttpClient().newCall(request).execute();
-                    if (response.isSuccessful())
-                        saveFile(response);
-                    return Observable.just(file);
-                } catch (Exception e) {
-                    return Observable.error(e);
-                }
-            }
-        })
-                .subscribeOn(Schedulers.io())
+        progressValueObservabele
+                .distinct()
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(file -> {
-                            Log.d("success", file.toString());
-                            imageView.setImageURI(Uri.parse(file.getPath()));
+                .subscribe(progressVal -> {
+                            arcProgress.setProgress(progressVal);
                         },
                         throwable -> {
                             Log.d("error", "error" + throwable.getMessage());
                         });
+
+        String destination = "/sdcard/downloadedfile.avi";
+        downloadTask("http://archive.blender.org/fileadmin/movies/softboy.avi", destination)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(success -> {
+                    resetDownloadButton();
+                    Intent intent = new Intent(android.content.Intent.ACTION_VIEW);
+                    File file = new File(destination);
+                    intent.setDataAndType(Uri.fromFile(file), "image");
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                }, error -> {
+                    Toast.makeText(getApplicationContext(), "Something went south", Toast.LENGTH_SHORT).show();
+                    resetDownloadButton();
+                });
     }
 
-    private void saveFile(Response response) {
-        file = new File(folder.getPath(), "/downloadedImage.png");
+
+    private Observable<Boolean> downloadTask(String source, String destination) {
+        return Observable.create(subscriber -> {
+            try {
+                boolean result = downloadFile(source, destination);
+                if (result) {
+                    subscriber.onNext(true);
+                    subscriber.onComplete();
+                } else {
+                    subscriber.onError(new Throwable("Download failed."));
+                }
+            } catch (Exception e) {
+                subscriber.onError(e);
+            }
+        });
+    }
+
+    private boolean downloadFile(String source, String destination) {
+        int count;
         try {
-            BufferedSink sink = Okio.buffer(Okio.sink(file));
-            sink.writeAll(response.body().source());
-            sink.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+            URL url = new URL(source);
+            URLConnection conection = url.openConnection();
+            conection.connect();
+            // this will be useful so that you can show a tipical 0-100% progress bar
+            int lenghtOfFile = conection.getContentLength();
+
+            // download the file
+            InputStream input = new BufferedInputStream(url.openStream(), 8192);
+
+            // Output stream
+            OutputStream output = new FileOutputStream(destination);
+
+            byte data[] = new byte[1024];
+
+            long total = 0;
+
+            while ((count = input.read(data)) != -1) {
+                total += count;
+                // publishing the progress....
+                // After this onProgressUpdate will be called
+                progressValueObservabele.onNext((int)((total*100)/lenghtOfFile));
+
+                // writing data to file
+                output.write(data, 0, count);
+            }
+
+            // flushing output
+            output.flush();
+
+            // closing streams
+            output.close();
+            input.close();
+            return true;
+
+        } catch (Exception e) {
+            Log.e("Error: ", e.getMessage());
         }
+
+        return false;
     }
 
 
